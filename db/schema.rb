08c5753c006c1051c4bd3e3e66c1c3900173bbc8
CJ <chasanid@gmail.com>
2018-02-08 00:36:12 -0500

    Creating Item Table/Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208045517) do
+ActiveRecord::Schema.define(version: 20180208052645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 20180208045517) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "price", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
